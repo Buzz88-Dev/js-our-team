6132d398c295321d4c0e0e465daad2f5e554c545
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-05-18 16:25:25 +0200

    aggiornamento

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 let teamContainer = document.querySelector(".team-container");
 console.log(teamContainer);
 
+
+
 let divTeamCard = document.createElement("div");
 divTeamCard.classList.add("team-card");
 console.log(divTeamCard);
 
-let cardText = document.createElement("div");
-cardText.classList.add("card-text");
-console.log(cardText);
+
+
+let cardImage = document.createElement("div");
+cardImage.classList.add("card-image");
 
 let myImage = document.createElement("img");
 console.log(myImage);
+
+
+
+let cardText = document.createElement("div");
+cardText.classList.add("card-text");
+console.log(cardText);
 
 let myh3 = document.createElement("h3");
 console.log(myh3);
@@ -66,9 +75,16 @@ for(let i = 0; i < myArrayObject.length; i++){
 
     for (let chiave in itemsObject){
         //console.log(chiave + ":" + itemsObject[chiave]);
-        console.log(itemsObject[chiave]);
+        //console.log(itemsObject["nome"]);
+        myImage.innerHTML = itemsObject["immagine"];
+        myh3.innerHTML = itemsObject["nome"];
+        myp.innerHTML = itemsObject["ruolo"];
+        
+        cardImage.append(myImage);
+        cardText.append(myh3, myp);
 
-        
-        
+        divTeamCard.append(cardImage, cardText);
+
+        teamContainer.append(divTeamCard);
     }
 }
