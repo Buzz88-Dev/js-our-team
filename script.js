06a1a9b69c4b9f4b6606c4f3193142ef53334cd3
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-05-18 16:02:56 +0200

    aggiornamento

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-let myDiv = document.querySelector(".team-container");
-console.log(myDiv);
+let teamContainer = document.querySelector(".team-container");
+console.log(teamContainer);
+
+let divTeamCard = document.createElement("div");
+divTeamCard.classList.add("team-card");
+console.log(divTeamCard);
+
+let cardText = document.createElement("div");
+cardText.classList.add("card-text");
+console.log(cardText);
+
+let myImage = document.createElement("img");
+console.log(myImage);
+
+let myh3 = document.createElement("h3");
+console.log(myh3);
+
+let myp = document.createElement("p");
+console.log(myp);
 
 // oggetto--> chiave : valore,
 let myArrayObject = [
@@ -48,7 +65,10 @@ for(let i = 0; i < myArrayObject.length; i++){
     let itemsObject = myArrayObject[i];
 
     for (let chiave in itemsObject){
-        console.log(chiave + ":" + itemsObject[chiave]);
+        //console.log(chiave + ":" + itemsObject[chiave]);
+        console.log(itemsObject[chiave]);
+
+        
         
     }
 }
