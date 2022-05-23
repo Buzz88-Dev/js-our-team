33f9dab5519967dcaa59608c02f85a987e346a91
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-05-23 16:41:13 +0200

    ended

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Viene fornito un layout di base in cui è presente una card di esempio inserita staticamente nell’html.
+// Questa card serve solo come timbro del markup necessario per ottenere il risultato finale e 
+// dovrà quindi essere rimossa dall’html.
+// Aggiungere un file js in cui definire un array di oggetti che rappresentano i membri del team.
+// Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell’html, 
+// stampare dinamicamente una card per ogni membro del team.
+
 
 // oggetto--> chiave : valore,
 let myArrayObject = [
@@ -55,14 +63,20 @@ let myButton = document.getElementById("addMemberButton");
 myButton.addEventListener("click",
     function(){
         let teamContainer = document.querySelector(".team-container");
+
         let divTeamCard = document.createElement("div");
         divTeamCard.classList.add("team-card");
+
         let cardImage = document.createElement("div");
         cardImage.classList.add("card-image");
+
         let myImage = document.createElement("img");
+
         let cardText = document.createElement("div");
         cardText.classList.add("card-text");
+
         let myh3 = document.createElement("h3");
+
         let myp = document.createElement("p");
 
         let nome = document.getElementById("name").value;
@@ -80,6 +94,8 @@ myButton.addEventListener("click",
         divTeamCard.append(cardImage, cardText);
 
         teamContainer.append(divTeamCard);
+
+        nome.innerHTML = " ";
         
     }
 )
@@ -87,21 +103,27 @@ myButton.addEventListener("click",
 
 for(let i = 0; i < myArrayObject.length; i++){
     let teamContainer = document.querySelector(".team-container");
+
     let divTeamCard = document.createElement("div");
     divTeamCard.classList.add("team-card");
+
     let cardImage = document.createElement("div");
     cardImage.classList.add("card-image");
+
     let myImage = document.createElement("img");
+
     let cardText = document.createElement("div");
     cardText.classList.add("card-text");
+
     let myh3 = document.createElement("h3");
+
     let myp = document.createElement("p");
 
     let itemsObject = myArrayObject[i];
 
     for (let chiave in itemsObject){
-        //console.log(chiave + ":" + itemsObject[chiave]);
-        //console.log(itemsObject["nome"]);
+        // console.log(chiave + ":" + itemsObject[chiave]); // mi stampa tutte le chiavi con il loro valore
+        // console.log(itemsObject["nome"]); // mi stampa i valori delle chiavi nome (è un ciclo)
         myImage.src = itemsObject["immagine"];
         myh3.innerHTML = itemsObject["nome"];
         myp.innerHTML = itemsObject["ruolo"];
@@ -116,6 +138,28 @@ for(let i = 0; i < myArrayObject.length; i++){
 }
 
 
-function richiamoElementi(){
-    
+
+/*
+    parte bonus del form
+*/
+
+// costanti del DOM
+const nameMember = document.getElementById('name');
+const roleMember = document.getElementById('role');
+const imgMember = document.getElementById('image');
+const addButton = document.getElementById('addMemberButton');
+addButton.addEventListener('click',addTeamCardObject);
+
+// funzioni del programma
+function addTeamCardObject(){
+    let cardObject = {
+        'nome' : nameMember.value,
+        'ruolo' : roleMember.value,
+        'foto' : imgMember.value
+    };
+    nameMember.value = '';
+    roleMember.value = '';
+    imgMember.value = '';
+    myArrayObject.push(cardObject);
+    addTeamCardDom(cardObject);
 }
